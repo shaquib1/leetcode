b4d61f5a4c52ed00a707f9d5b62f385e0ea3621b
Md Shaquib Alam <shaquib475@gmail.com>
2023-05-20 00:28:46 +0530

    Reverse String

---
+++ 344_Reverse_String.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n=s.size();
+    int start=0;
+    int end=n-1;
+
+    while(start<=end){
+        swap(s[start],s[end]);
+        start++;
+        end--;
+    }
+
+
+    }
+};
