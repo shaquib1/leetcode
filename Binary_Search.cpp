5e0e16181bb91d4908256349094f5443174ebfb4
Md Shaquib Alam <shaquib475@gmail.com>
2023-05-22 01:25:19 +0530

    Binary Search

---
+++ Binary_Search.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+
+        int n= nums.size();
+        int s=0; 
+        int e=n-1;
+         int mid=(s+e)/2;
+
+         while(s<=e){
+
+             if(nums[mid]==target){
+                 return mid;
+
+             }
+             if(nums[mid]<target){
+                 s=mid+1;
+             }else{
+                 e=mid-1;
+             }
+             mid=(s+e)/2;
+         }
+         return -1;
+        
+    }
+};
