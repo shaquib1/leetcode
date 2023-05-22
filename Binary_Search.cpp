7b03923f6363e5998c1152541c732d58e2b482ea
Md Shaquib Alam <shaquib475@gmail.com>
2023-05-23 00:29:47 +0530

    Insertion Sort

--- Binary_Search.cpp
+++ Binary_Search.cpp
@@ -1,26 +1,31 @@
-class Solution {
+class Solution
+{
 public:
-    int search(vector<int>& nums, int target) {
+    int search(vector<int> &nums, int target)
+    {
 
-        int n= nums.size();
-        int s=0; 
-        int e=n-1;
-         int mid=(s+e)/2;
+        int n = nums.size();
+        int s = 0;
+        int e = n - 1;
+        int mid = (s + e) / 2;
 
-         while(s<=e){
+        while (s <= e)
+        {
 
-             if(nums[mid]==target){
-                 return mid;
-
-             }
-             if(nums[mid]<target){
-                 s=mid+1;
-             }else{
-                 e=mid-1;
-             }
-             mid=(s+e)/2;
-         }
-         return -1;
-        
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            if (nums[mid] < target)
+            {
+                s = mid + 1;
+            }
+            else
+            {
+                e = mid - 1;
+            }
+            mid = (s + e) / 2;
+        }
+        return -1;
     }
 };
