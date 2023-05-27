1f30a42deee057e66f6f13960faeaf3681a38ef4
Md Shaquib Alam <shaquib475@gmail.com>
2023-05-27 23:44:29 +0530

    Merge Sorted Array

--- 88_Merge_Sorted_Array.cpp
+++ 88_Merge_Sorted_Array.cpp
@@ -1,1 +1,16 @@
 // in this we know how to merge two shorted array
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+
+         int f=0;
+       for(int i = m;i<m+n;i++){
+            nums1[i]=nums2[f];
+            f++;
+        }
+        sort(nums1.begin(),nums1.end());
+        
+    }
+};
