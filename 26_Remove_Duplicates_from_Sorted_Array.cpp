a31db34d650289f3ee534e6378c8a0629194f499
Md Shaquib Alam <shaquib475@gmail.com>
2023-06-02 20:40:38 +0530

    Remove Element from an Array

--- 26_Remove_Duplicates_from_Sorted_Array.cpp
+++ 26_Remove_Duplicates_from_Sorted_Array.cpp
@@ -1,10 +1,14 @@
-class Solution {
+class Solution
+{
 public:
-    int removeDuplicates(vector<int>& nums) {
-        
-         int j = 1;
-        for (int i = 0; i < nums.size() - 1; i++) {
-            if (nums[i] != nums[i+1]) nums[j++] = nums[i+1];
+    int removeDuplicates(vector<int> &nums)
+    {
+
+        int j = 1;
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            if (nums[i] != nums[i + 1])
+                nums[j++] = nums[i + 1];
         }
         return j;
     }
