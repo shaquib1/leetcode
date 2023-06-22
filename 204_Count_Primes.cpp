a914fffbbbc151dd4e7751b73ee868ee01854718
Md Shaquib Alam <shaquib475@gmail.com>
2023-06-22 22:05:03 +0530

    Count Primes

--- 204_Count_Primes.cpp
+++ 204_Count_Primes.cpp
@@ -1,29 +1,21 @@
 class Solution {
+public:
+    int countPrimes(int n) {
+        
+        int count =0;
+        vector<bool> prime(n+1 , true);
 
-    private:
-    bool isPrime(int n){
-
-        if(n<=1)
-        return false;
+        prime[0]=prime[1]=false;
 
         for(int i=2; i<n; i++){
-            if(n%i==0){
-                return false;
+            if(prime[i]){
+                count++;
+            
+            for(int j=2*i; j<n; j=j+i){
+                prime[j]=0;
+            }
             }
         }
-        return true;
-    }
-public:
-  
-    int countPrimes(int n) {
-
-          int count=0;
-
-          for(int i=2; i<n; i++){
-              if(isPrime(i)){
-                  count++;
-              }
-          }
-          return count;
+        return count;
     }
 };
