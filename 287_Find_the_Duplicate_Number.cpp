1eab6efb1f990f128558b06b6412a3b29b1a7f1d
Md Shaquib Alam <shaquib475@gmail.com>
2023-07-07 14:15:18 +0530

    find duplicate number

---
+++ 287_Find_the_Duplicate_Number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+        int n=nums.size();
+        int ans;
+
+
+        sort(nums.begin(),nums.end());
+        for(int i=0; i<n-1;i++){
+            if(nums[i]==nums[i+1]){
+                ans=nums[i];
+            }
+            
+        }
+    
+       return ans;
+    }
+};
