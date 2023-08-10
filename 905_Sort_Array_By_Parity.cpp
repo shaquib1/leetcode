16661666711b6aa0b76573b9ba5b93669421617d
Md Shaquib Alam <shaquib475@gmail.com>
2023-08-10 11:47:51 +0530

    sort array by parity

---
+++ 905_Sort_Array_By_Parity.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums){
+        
+        int n = nums.size();
+         vector<int>ans;
+        
+  
+        for(int i=0; i<n; i++){
+            if(nums[i]%2==0){
+                ans.push_back(nums[i]);
+            }
+        }
+        
+         for(int i=0; i<n; i++){
+            if(nums[i]%2 !=0){
+                ans.push_back(nums[i]);
+            }
+        }
+        return ans;
+    }
+};
