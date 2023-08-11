d0e6019c90f01282efff36ee31a0d1e93aab64bb
Md Shaquib Alam <shaquib475@gmail.com>
2023-08-11 08:39:28 +0530

    find the differece

---
+++ 389_Find_the_Difference.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        
+        int c = 0; 
+        
+        for(int i=0; i<s.size(); i++){
+            c^=s[i];
+        }
+        
+        for(int j=0; j<t.size(); j++){
+            c^=t[j];
+        }
+        
+        return c;
+    }
+};
