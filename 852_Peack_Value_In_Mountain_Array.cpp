35a91c9ee394511e21d37cdd7a162a0a6c0891c7
Md Shaquib Alam <shaquib475@gmail.com>
2023-08-13 19:11:56 +0530

    peak value in mountain array

--- 852_Peack_Value_In_Mountain_Array.cpp
+++ 852_Peack_Value_In_Mountain_Array.cpp
@@ -1,20 +1,25 @@
-class Solution {
+class Solution
+{
 public:
-    int peakIndexInMountainArray(vector<int>& arr) {
+    int peakIndexInMountainArray(vector<int> &arr)
+    {
 
-        int s=0; 
-        int e=arr.size()-1;
-        int m= (s+e)/2;
+        int s = 0;
+        int e = arr.size() - 1;
+        int m = (s + e) / 2;
 
-        while(s<e){
-            if(arr[m]<arr[m+1]){
-                s=m+1;
-            }else {
-                e=m;
+        while (s < e)
+        {
+            if (arr[m] < arr[m + 1])
+            {
+                s = m + 1;
             }
-            m=(s+e)/2;
+            else
+            {
+                e = m;
+            }
+            m = (s + e) / 2;
         }
         return s;
-        
     }
 };
