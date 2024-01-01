b8c95a74add9c6f6140d603fda212557f39d3e0f
Md Shaquib Alam <shaquib475@gmail.com>
2024-01-01 23:14:26 +0530

    Remove duplicate from shorted array

--- 26_Remove_Duplicates_from_Sorted_Array.cpp
+++ 26_Remove_Duplicates_from_Sorted_Array.cpp
@@ -1,15 +1,17 @@
-class Solution
-{
+class Solution {
 public:
-    int removeDuplicates(vector<int> &nums)
-    {
-
-        int j = 1;
-        for (int i = 0; i < nums.size() - 1; i++)
-        {
-            if (nums[i] != nums[i + 1])
-                nums[j++] = nums[i + 1];
+    int removeDuplicates(vector<int>& nums) {
+        
+         int i=0;
+        for(int j=1; j<nums.size(); j++){
+            if(nums[i]!=nums[j]){
+             i++;
+             nums[i]=nums[j];
+            }
+            
+            
         }
-        return j;
+
+        return i+1;
     }
 };
